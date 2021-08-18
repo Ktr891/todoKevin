222c0e80fd60b87e61aee05b40306666a75cdaad
KEVIN TORRES RIVERA <kevintorres259397@correo.itm.edu.co>
2021-08-17 23:20:50 -0500

    Add entity

---
+++ todoKevin.common/Models/todo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace todoKevin.common.Models
+{
+    public class todo
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
